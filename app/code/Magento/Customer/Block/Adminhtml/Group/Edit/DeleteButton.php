a225ef10218e0dd34c60d9ace7c9b4712698489d
Lewis Voncken <lewis@experius.nl>
2018-11-07 19:16:45 +0100

    [TASK] Moved Buttons config from class to Ui Component

--- app/code/Magento/Customer/Block/Adminhtml/Group/Edit/DeleteButton.php
+++ app/code/Magento/Customer/Block/Adminhtml/Group/Edit/DeleteButton.php
@@ -5,6 +5,7 @@
  */
 namespace Magento\Customer\Block\Adminhtml\Group\Edit;
 
+use Magento\Backend\Block\Widget\Context;
 use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
 
 /**
@@ -12,8 +13,42 @@ use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
  *
  * @package Magento\Customer\Block\Adminhtml\Group\Edit
  */
-class DeleteButton extends GenericButton implements ButtonProviderInterface
+class DeleteButton implements ButtonProviderInterface
 {
+    /**
+     * @var Context
+     */
+    protected $context;
+
+    /**
+     * @param \Magento\Backend\Block\Widget\Context $context
+     */
+    public function __construct(Context $context)
+    {
+        $this->context = $context;
+    }
+
+    /**
+     * Return model ID
+     *
+     * @return int|null
+     */
+    public function getModelId()
+    {
+        return $this->context->getRequest()->getParam('group_id');
+    }
+
+    /**
+     * Generate url by route and parameters
+     *
+     * @param   string $route
+     * @param   array $params
+     * @return  string
+     */
+    public function getUrl($route = '', $params = [])
+    {
+        return $this->context->getUrlBuilder()->getUrl($route, $params);
+    }
 
     /**
      * @return array
